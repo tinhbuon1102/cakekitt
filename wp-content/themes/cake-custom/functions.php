9c2ef2b0ce45e761a6aeef54ea58a2802195709b
thangtqvn <quocthang.2001@gmail.com>
2017-03-07 16:53:51 +0700

    Upload files

--- wp-content/themes/cake-custom/functions.php
+++ wp-content/themes/cake-custom/functions.php
@@ -272,7 +272,6 @@ function cake_steps_store(){
 		{
 			foreach ($_SESSION['cake_custom_order_step_' . $i] as $fieldName => $fieldValue)
 			{
-				pr($_SESSION['cake_custom_order_step_' . $i]);die;
 				if (strpos($fieldName, 'custom_order_') === false) continue;
 				
 				$fieldValue = is_array($fieldMapping[$fieldName]['value'][$fieldValue]) ? $fieldMapping[$fieldName]['value'][$fieldValue] : (is_array($fieldMapping[$fieldName]['value']) ? $fieldMapping[$fieldName]['value'][$fieldValue] : $fieldValue);
