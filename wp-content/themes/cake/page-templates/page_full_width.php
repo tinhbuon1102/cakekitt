779f679b0e52a6b0c58532d0038c082c97f2af98
tinhbuon1102 <quocthang.2001@gmail.com>
2017-03-14 05:31:49 +0900

    Server Upload files

--- wp-content/themes/cake/page-templates/page_full_width.php
+++ wp-content/themes/cake/page-templates/page_full_width.php
@@ -6,12 +6,19 @@
  * @author		Codeopus <support.codeopus.net>
  * Websites		http://codeopus.net
  */
-get_header(); ?>
+get_header();?>
 
 	
 	<?php get_template_part('inc/page-header');	?>
+	<?php
+$page = get_post( get_the_ID() );
+// 現在表示しているページの投稿IDから投稿情報を取得
 
-	<div id="content-wrapper">
+$slug = $page->post_name;
+// 投稿のスラッグを取得
+?>
+
+	<div id="content-wrapper" class="page-<?php echo $slug; ?>">
 	<div class="container expand">
 	
 			<div id="primary" class="content-area">
