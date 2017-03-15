6c2d35f59b199727eece0e5779688e82ec3464c9
thangtqvn <quocthang.2001@gmail.com>
2017-03-15 13:36:02 +0700

    Upload files

--- wp-content/themes/cake/woocommerce/emails/customer-invoice.php
+++ wp-content/themes/cake/woocommerce/emails/customer-invoice.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<?php if ( $order->has_status( 'pending' ) ) : ?>
+<?php if ( $order->has_status( 'pending' ) || $order->has_status( 'accepted' ) ) : ?>
 	<p><?php printf( __( 'An order has been created for you on %s. To pay for this order please use the following link: %s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
 <?php endif; ?>
 
@@ -53,4 +53,3 @@ do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_
  * @hooked WC_Emails::email_footer() Output the email footer
  */
 do_action( 'woocommerce_email_footer', $email );
-die('x');
