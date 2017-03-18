5e5eefc4a40020c32ae3489d60187a2336444630
tinhbuon1102 <quocthang.2001@gmail.com>
2017-03-18 12:26:27 +0900

    Server Upload files

--- wp-content/themes/cake-custom/js/woo-custom.js
+++ wp-content/themes/cake-custom/js/woo-custom.js
@@ -8,10 +8,12 @@ $(function() {
 	$('input#password').wrap('<span class="pass"></span>');
 	//$('input#reg_password').wrap('<span class="pass"></span>');
 	$('input#reg_email').wrapAll('<span class="email"></span>');
-	$('.checkout > h3#order_review_heading, .checkout > #order_review').wrapAll('<div class="col-md-4 columns position-static pt-md-4 pt-sm-2 pb-sm-4"></div>');
+	$('.checkout > h3#order_review_heading, .checkout > #order_review').wrapAll('<div class="inner"></div>');
+	$('.checkout .inner').wrapAll('<div class="col-md-4 columns position-static pt-md-4 pt-sm-2 pb-sm-4"></div>');
 	$('.checkout > #customer_details, .checkout > ul').wrapAll('<div class="col-md-8 columns"></div>');
 	$('.checkout > .col-md-8 > ul').wrap('<div class="select-datetime"></div>');
 	$('.checkout > .columns').wrapAll('<div class="row" id="checkoutbox"></div>');
+	$(".cake_info_wraper > .row:has(.col-xs-4)").addClass('first-child');
 	//$("span.pass:not(:has(.woocommerce-password-hint))").removeClass('pass');
 	$('#customer_login > div.u-column1 > h2, #customer_login > div.u-column1 > form').wrapAll('<div class="inner"></div>');
 	$('#customer_login > div.u-column2 > h2, #customer_login > div.u-column2 > form').wrapAll('<div class="inner"></div>');
