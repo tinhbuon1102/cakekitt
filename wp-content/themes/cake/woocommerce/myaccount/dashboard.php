99db42873995d83763c0f05ecfff17be47464a22
tinhbuon1102 <quocthang.2001@gmail.com>
2017-03-19 11:49:05 +0900

    Server Upload files

--- wp-content/themes/cake/woocommerce/myaccount/dashboard.php
+++ wp-content/themes/cake/woocommerce/myaccount/dashboard.php
@@ -22,7 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-
+<h1 class="mb-5">My Account</h1>
+<div class="account-logout">
+	<a class="button simple" title="Log out" href="<?php echo wp_logout_url( get_permalink( woocommerce_get_page_id( 'myaccount' ) ) );?>"><?php _e( 'Log out', 'woocommerce' ); ?></a>
+</div>
+<div class="account-welcome">
 <p>
 	<?php
 		echo sprintf( esc_attr__( 'Hello %s%s%s (not %2$s? %sSign out%s)', 'woocommerce' ), '<strong>', esc_html( $current_user->display_name ), '</strong>', '<a href="' . esc_url( wc_logout_url( wc_get_page_permalink( 'myaccount' ) ) ) . '">', '</a>' );
@@ -34,7 +38,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 		echo sprintf( esc_attr__( 'From your account dashboard you can view your %1$srecent orders%2$s, manage your %3$sshipping and billing addresses%2$s and %4$sedit your password and account details%2$s.', 'woocommerce' ), '<a href="' . esc_url( wc_get_endpoint_url( 'orders' ) ) . '">', '</a>', '<a href="' . esc_url( wc_get_endpoint_url( 'edit-address' ) ) . '">', '<a href="' . esc_url( wc_get_endpoint_url( 'edit-account' ) ) . '">' );
 	?>
 </p>
-
+</div>
+<div class="account-box account-order-history clearfix fright">
+	<h3 class="section-header"><?php _e( 'Order History', 'woocommerce' ); ?></h3>
+	<div class="account-box-content clearfix">
+		<div class="account-box-image"><i class="linericon-clipboard-text"></i></div>
+		<p><?php _e( 'You haven’t placed any orders yet', 'woocommerce' ); ?></p>
+		
+	</div>
+</div>
+<div class="account-box account-details clearfix fleft">
+	<h3 class="section-header"><?php _e( 'Account Details', 'woocommerce' ); ?><a class="section-header-note" href="#"><?php _e( 'View details', 'woocommerce' ); ?></a></h3>
+	<div class="account-box-content clearfix">
+	<div class="account-box-image"><i class="linericon-clipboard-user"></i></div>
+		<p class="account-box-label">Lastname Firstname</p>
+		<p>1989.11.22</p>
+		<p>kyoooko1122@icloud.com</p>
+	</div>
+</div>
+<div class="account-box account-details clearfix fleft">
+	<h3 class="section-header"><?php _e( 'Shipping info', 'woocommerce' ); ?><a class="section-header-note" href="#"><?php _e( 'View details', 'woocommerce' ); ?></a></h3>
+	<div class="account-box-content clearfix">
+	<div class="account-box-image"><i class="linericon-truck"></i></div>
+		<p class="account-box-label">Store Name</p>
+		<p>postcode</p>
+		<p>PrefCityAddress1Address2</p>
+	</div>
+</div>
 <?php
 	/**
 	 * My Account dashboard.
