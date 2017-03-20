d0ee950f7b34b1c51a28d96034f91e047aeceedc
thangtqvn <quocthang.2001@gmail.com>
2017-03-20 14:55:47 +0700

    Upload files

--- wp-content/themes/cake/woocommerce/emails/email-customer-details.php
+++ wp-content/themes/cake/woocommerce/emails/email-customer-details.php
@@ -21,11 +21,5 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+// thangtq remove
 ?>
-<h2><?php _e( 'Customer details', 'woocommerce' ); ?></h2>
-<ul>
-    <?php foreach ( $fields as $field ) : ?>
-        <li><strong><?php echo wp_kses_post( $field['label'] ); ?>:</strong> <span class="text"><?php echo wp_kses_post( $field['value'] ); ?></span></li>
-    <?php endforeach; ?>
-</ul>
