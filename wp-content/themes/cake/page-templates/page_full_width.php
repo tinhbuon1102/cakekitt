c46d951d9e4d7f9bff72c9c7beab4fce49dbd499
tinhbuon1102 <quocthang.2001@gmail.com>
2017-12-18 10:53:45 +0900

    Deploy files From Test Environment

--- wp-content/themes/cake/page-templates/page_full_width.php
+++ wp-content/themes/cake/page-templates/page_full_width.php
@@ -24,15 +24,7 @@ $slug = $page->post_name;
 			<div id="primary" class="content-area">
 					<main id="main" class="site-main">
 					<?php while ( have_posts() ) : the_post(); ?>
-					<?php if(is_page('access') && !current_user_can('administrator') ):  ?>
-					<div id="content-wrapper">
-					<div class="coming-container container">
-					<h1>Coming Soon...</h1>
-					</div>
-					</div>
-					<?php else: ?>
 					<?php get_template_part( 'content', 'page' ); ?>
-					<?php endif; ?>
 							
 										
 						<?php endwhile; // end of the loop. ?>
