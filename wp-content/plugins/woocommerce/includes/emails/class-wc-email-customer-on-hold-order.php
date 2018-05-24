ac589e13827a79c484dd657c7033fc9d20b28446
thangtqvn <quocthang.2001@gmail.com>
2018-05-24 09:42:55 +0700

    Upload files

--- wp-content/plugins/woocommerce/includes/emails/class-wc-email-customer-on-hold-order.php
+++ wp-content/plugins/woocommerce/includes/emails/class-wc-email-customer-on-hold-order.php
@@ -33,7 +33,7 @@ class WC_Email_Customer_On_Hold_Order extends WC_Email {
 		$this->template_plain   = 'emails/plain/customer-on-hold-order.php';
 
 		// Triggers for this email
-		add_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $this, 'trigger' ) );
+		//add_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $this, 'trigger' ) );
 		add_action( 'woocommerce_order_status_failed_to_on-hold_notification', array( $this, 'trigger' ) );
 
 		// Call parent constructor
